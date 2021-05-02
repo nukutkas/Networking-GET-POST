0adb91aa52451c2c0fe386c752ed9a9a6eb7750e
Nikita Kochetov <nothsmple@mail.ru>
2021-05-02 15:12:51 +0300

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Татьяна Кочетова on 02.05.2021.
+//  Copyright © 2021 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func getRequest(_ sender: Any) {
+    }
+    
+    @IBAction func postRequest(_ sender: Any) {
+    }
+    
+}
+
